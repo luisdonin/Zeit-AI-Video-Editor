efd1a8a380093815569047822a91ca1c25c4fd85
J. L. B. Donin <88415069+luisdonin@users.noreply.github.com>
2023-09-29 21:34:07 -0300

    Editor.py

--- editor.py
+++ editor.py
@@ -1,16 +1,11 @@
-from pathlib import Path
-import os
-suffix = ".mp4"
-input_path= Path.home() / "Desktop/foo"
-file_paths= [subp for subp in input_path.rglob('*') if  suffix == subp.suffix]
-file_paths.sort()
+import ffmpeg
 
-output_path =  Path.home() / "Desktop/foo/new"
-output_path.mkdir(parents=True, exist_ok=True)
 
-for file_p in file_paths:
-    input = str(file_p)
-    output = str(  output_path / file_p.name  ) 
-    command = f"ffmpeg -sseof -10 -i {input} {output}"
-    print(command)
-    os.system(command)
+start_time = '00:00:10' # Start time for trimming (HH:MM:SS)
+end_time = '00:00:20' # End time for trimming (HH:MM:SS)
+
+(
+	ffmpeg.input("input.mp4", ss=start_time, to=end_time)
+	.output("trimmed_output.mp4")
+	.run()
+)
