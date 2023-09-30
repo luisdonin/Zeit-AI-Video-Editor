940045dd4e18b0f0e7b0728986db67de41804078
Zanchet2020 <lucasamuel040@gmail.com>
2023-09-30 00:01:16 -0300

    Added trim_video.py

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const hostname = "localhost"
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  res.sendFile('index.html')
+})
+
+
+server.listen(port, hostname)
+
+
+// const http = require('node:http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// }); 
