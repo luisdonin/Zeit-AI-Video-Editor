ab1b50e37ff5e295ae2cdf83cff7c9087db4ba09
Zanchet2020 <lucasamuel040@gmail.com>
2023-10-03 15:06:45 -0300

    Added ytdl module and binded to Get Clips button

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const app = express()
-const http = require('http')
 const hostname = "localhost"
+const cors = require('cors')
+const ytdl = require('ytdl-core')
 const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.sendFile('index.html')
-})
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/download', (req,res) => {
+  var URL = req.query.URL;
+  res.json({url: URL});
+});
 
 
-server.listen(port, hostname)
+app.listen(port, () => {
+  console.log(`Example app listening at http://${hostname}:${port}`)
+});
+
+
 
 
 // const http = require('node:http');
