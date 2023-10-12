0816b5582cf96694b305c4efd7e76a8dd47b9da8
Zanchet2020 <lucasamuel040@gmail.com>
2023-10-12 03:02:58 -0300

    Added server downloader module and client request

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,10 +1,8 @@
+const downloader = require('./server_downloader.js');
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const path = require('path')
-const ytdl = require('ytdl-core')
-const fs = require('fs')
-const ffmpeg = require('fluent-ffmpeg')
 
 const port = 3000
 const hostname = "localhost"
@@ -26,78 +24,12 @@ app.get('/download', (req,res) => {
 	const videoFilePath = "../../videos/inputs/video.mp4";
 	const mergedFilePath = "../../videos/inputs/merged.mp4";
 
-
-	ytdl.getInfo(videoURL).then(info => {
-    	// gets all the available formats up to 1080p
-    	const videoFormats = info.formats.filter(format => 
-        		format.hasVideo && 
-        	(
-        		format.qualityLabel === "1080p" || 
-        		format.qualityLabel === "720p" || 
-        		format.qualityLabel === "480p" || 
-        		format.qualityLabel === "360p" || 
-        		format.qualityLabel === "240p" || 
-        		format.qualityLabel === "144p"
-        	));
-    
-    	const audioFormats = info.formats.filter(format => format.hasAudio);
-
-    	const audioFormat = ytdl.chooseFormat(audioFormats, { quality: 'highestaudio' , format: audioFormatPredefined });
-    	// downloads the highest available format up to 1080p
-    	const videoFormat = ytdl.chooseFormat(videoFormats, { quality: 'highestvideo' , format: videoFormatPredefined });
-    
-    	const videoStream = ytdl(videoURL, { format: videoFormat });
-    	const audioStream = ytdl(videoURL, { format: audioFormat });
-
-    	videoStream.pipe(fs.createWriteStream(videoFilePath));
-    	audioStream.pipe(fs.createWriteStream(audioFilePath));
-
-    	let finishedStreams = 0;
-
-    	audioStream.on('end', () => {
-    		console.log("Audio download finished");
-    		console.log(`Saved to '${audioFilePath}'`);
-    		finishedStreams++;
-    		if (finishedStreams === 2) {
-    			mergeAudioAndVideo();
-    		}
-    	});
-
-    	videoStream.on('end', () => {
-    		console.log("Video download finished");
-    		console.log(`Saved to '${videoFilePath}'`);
-    		finishedStreams++;
-    		if (finishedStreams === 2) {
-    			mergeAudioAndVideo();
-    		}
-    	});
-
-    	const mergeAudioAndVideo = () => {
-			console.log("Merging audio and video.....")
-    		ffmpeg()
-    			.input(audioFilePath)
-    	    	.input(videoFilePath)
-    	    	.output(mergedFilePath)
-    	    	.videoCodec('libx264')
-    	    	.audioCodec('aac')
-    	    	.on('end', () => {
-    	    		console.log(`Finished merging audio and video, saved to '${mergedFilePath}'`);
-    	    	})
-    	    	.on('error', (err) => {
-    	    		console.error(err);
-    	    	})
-    	    	.run();
-    	};	
-	
-    	videoStream.on('error', (err) => {
-    		console.error(err);
-    	});
-  	}).catch(err => {
-    	console.error(err);
-    });
+	downloader.downloadVideo(videoURL, videoFilePath, audioFilePath, mergedFilePath, videoFormatPredefined, audioFormatPredefined);
 
   	res.header('Content-Disposition', `attachment; filename="video.mp4"`);
 });
+
+
 
 app.listen(port, () => {
   	console.log(`Server listening at http://${hostname}:${port}`)
